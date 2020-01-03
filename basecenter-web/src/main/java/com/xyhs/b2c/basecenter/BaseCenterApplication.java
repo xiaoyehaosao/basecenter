5365d658085365f62d8962d2f3eb6bd8f765014c
lijiapeng <806626194@qq.com>
2020-01-03 17:48:59 +0800

    修改配置

--- basecenter-web/src/main/java/com/xyhs/b2c/basecenter/BaseCenterApplication.java
+++ basecenter-web/src/main/java/com/xyhs/b2c/basecenter/BaseCenterApplication.java
@@ -1,6 +1,5 @@
 package com.xyhs.b2c.basecenter;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 15:07 2019/11/13
  **/
 @SpringBootApplication
-@MapperScan("com.xyhs.b2c.basecenter.mapper")
 public class BaseCenterApplication {
 
     public static void main(String[] args) {
